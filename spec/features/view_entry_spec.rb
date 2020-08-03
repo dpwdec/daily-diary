ebc16dc3b071eab4506822c65f13b03a9c6c28ec
dpwdec <declankolakowski@gmail.com>
2020-08-03 15:52:54 +0100

    update before deletion

--- spec/features/view_entry_spec.rb
+++ spec/features/view_entry_spec.rb
@@ -5,4 +5,12 @@ feature 'view a specific entry' do
     click_link('Day 2 my first pikachu')
     expect(page).to have_content('On my second day I met a pikachu!')
   end
+
+  scenario 'user can return from entry to entries page' do
+    populate_database
+    visit('/entries')
+    click_link('Day 2 my first pikachu')
+    click_link('back')
+    expect(page).to have_current_path('/entries')
+  end
 end
